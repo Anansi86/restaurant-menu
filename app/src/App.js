b325ed1087be6043eebbe84a24545bff748cb257
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-10-17 05:37:16 +0000

    header

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Accordion from './components/Accordion';
 import axios from 'axios';
+import GetHeader from './components/Header';
 
 function App() {
    
+  
     //const [menuItems, setMenuItems] = useState;
     const [data, setData] = useState([]);
     useEffect(() => {
@@ -12,14 +14,22 @@ function App() {
           const response = await axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/');
           setData(response.data);
         }
-        getData();
-      }, []);
-        console.log(data)
+        getData()
 
-    const myAccordion = data.map(item => <Accordion  item={item} />);
-  
-       return <h1> {myAccordion} </h1>;
-    
+      }, []);
+        
+
+        const myAccordion = data.map(item => <Accordion  item={item} />);
+         
+       
+        return (
+            <div className='container'>
+                 <GetHeader />
+                 <div className='container'>
+                <h1> {myAccordion} </h1>
+                </div>
+            </div>
+        )
     }
     
     export default App
